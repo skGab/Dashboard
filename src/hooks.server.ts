a5cfbdbe0bc5eaf34ff3b3760e890595d38cb7cd
skGab <gabriel.wti02@gmail.com>
2023-11-24 14:55:59 -0300

    Disable Login

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,13 @@
-import { SvelteKitAuth } from '@auth/sveltekit';
-import GoogleProvider from '@auth/core/providers/google';
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, AUTH_SECRET } from '$env/static/private';
+// import { SvelteKitAuth } from '@auth/sveltekit';
+// import GoogleProvider from '@auth/core/providers/google';
+// import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, AUTH_SECRET } from '$env/static/private';
 
-export const handle = SvelteKitAuth({
-	trustHost: true,
-	secret: AUTH_SECRET,
-	providers: [GoogleProvider({ clientId: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_CLIENT_SECRET })],
-	pages: {
-		signIn: '/login',
-		signOut: '/logout'
-	}
-});
+// export const handle = SvelteKitAuth({
+// 	trustHost: true,
+// 	secret: AUTH_SECRET,
+// 	providers: [GoogleProvider({ clientId: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_CLIENT_SECRET })],
+// 	pages: {
+// 		signIn: '/login',
+// 		signOut: '/logout'
+// 	}
+// });
--- src/routes/terminal/+page.server.ts
+++ src/routes/terminal/+page.server.ts
@@ -1,13 +1,13 @@
-import { redirect } from '@sveltejs/kit';
+// import { redirect } from '@sveltejs/kit';
 
-export const load = async (event) => {
-	const session = await event.locals.getSession();
+// export const load = async (event) => {
+// 	const session = await event.locals.getSession();
 
-	if (!session) {
-		throw redirect(307, 'auth/signin');
-	}
+// 	if (!session) {
+// 		throw redirect(307, 'auth/signin');
+// 	}
 
-	return {
-		session
-	};
-};
+// 	return {
+// 		session
+// 	};
+// };
